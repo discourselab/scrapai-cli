059708bf1f733e7932c316ce214748b70e75c03a
Ranu <neneranadheer@gmail.com>
2026-02-19 19:16:17 +0000

    Fix HTTP11DownloadHandler to use crawler object
    HTTP11DownloadHandler expects the crawler object (not settings).
    Pass self.crawler instead of self.crawler.settings.

--- handlers/cloudflare_handler.py
+++ handlers/cloudflare_handler.py
@@ -95,7 +95,7 @@ class CloudflareDownloadHandler:
         # If CF not enabled, use default Scrapy downloader
         if not cf_enabled:
             from scrapy.core.downloader.handlers.http11 import HTTP11DownloadHandler
-            handler = HTTP11DownloadHandler(self.crawler.settings)
+            handler = HTTP11DownloadHandler(self.crawler)
             return handler.download_request(request, spider)
 
         # CF enabled - use browser
